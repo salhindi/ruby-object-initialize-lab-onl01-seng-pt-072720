cae3216c5d3b15dd3d3edb6f05c4999568f92780
Sara A <calisaraalhindi@gmail.com>
2020-08-15 20:39:26 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,15 @@
+class Person
+  def initialize(persons_name)
+    @name= (persons_name)
+  end
+  
+  def name= (person_name)
+   @name = persons_name
+ end
+ 
+ def name
+   @name
+ end
+ end
+
+beyonce = Person.new("Beyonce")
